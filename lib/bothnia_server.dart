201ff25c0b4cc4783d2d63c3bef5f303ac662cb1
christianmartensson <christian.martonens@gmail.com>
2020-04-10 12:29:23 +0200

    init

---
+++ lib/bothnia_server.dart
@@ -0,0 +1,11 @@
+/// bothnia_server
+///
+/// A Aqueduct web server.
+library bothnia_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
---
+++ test/harness/app.dart
@@ -0,0 +1,32 @@
+import 'package:bothnia_server/bothnia_server.dart';
+import 'package:aqueduct_test/aqueduct_test.dart';
+
+export 'package:bothnia_server/bothnia_server.dart';
+export 'package:aqueduct_test/aqueduct_test.dart';
+export 'package:test/test.dart';
+export 'package:aqueduct/aqueduct.dart';
+
+/// A testing harness for bothnia_server.
+///
+/// A harness for testing an aqueduct application. Example test file:
+///
+///         void main() {
+///           Harness harness = Harness()..install();
+///
+///           test("GET /path returns 200", () async {
+///             final response = await harness.agent.get("/path");
+///             expectResponse(response, 200);
+///           });
+///         }
+///
+class Harness extends TestHarness<BothniaServerChannel> {
+  @override
+  Future onSetUp() async {
+
+  }
+
+  @override
+  Future onTearDown() async {
+
+  }
+}
