78bfc9122f6cfcabc7452a1edbe5f6760a9efb1f
christianmartensson <christian.martonens@gmail.com>
2020-04-23 08:17:58 +0200

    changed type of controller

--- lib/controllers/image_controller.dart
+++ lib/controllers/image_controller.dart
@@ -1,6 +1,6 @@
 import 'package:aqueduct/aqueduct.dart';
 import 'package:bothnia_server/bothnia_server.dart';
 
-class ImageController extends ManagedObjectController<ImageMetaData> {
+class ImageController extends ManagedObjectController<Image> {
   ImageController(ManagedContext context) : super(context);
 }
