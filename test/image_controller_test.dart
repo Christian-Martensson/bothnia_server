92c06fe71e6038fd8858ca942cace0fbf5190a5f
Christian Mårtensson <chris.martensson@pm.me>
2020-05-11 13:49:41 +0200

    test

--- test/image_controller_test.dart
+++ test/image_controller_test.dart
@@ -152,54 +152,38 @@ Future main() async {
 
     expect(
         res,
-        hasResponse(
-          200,
-        ));
+        hasResponse(200, body: {
+          "id": isInteger,
+          "name": "My Picture",
+          "description": isNull,
+          "base64": isNull,
+          "resolution": isNull,
+          "created": isString,
+          "modified": isString,
+          "captured": isNull,
+          "firstPubDate": isNull,
+          "xCoordinates": isNull,
+          "yCoordinates": isNull,
+          "license": isNull,
+          "usesLeft": isNull,
+          "isPublicallyAdded": false,
+          "photographer": {
+            "id": isInteger,
+            "fName": "Chris",
+            "lName": "Mar",
+            "email": isNull,
+            "phone": isNull,
+          },
+          "user": {
+            "id": isInteger,
+            "username": "chrmrt",
+            "name": "Christian Mårtensson",
+            "type": "photographer",
+            "canEditor": isFalse,
+            "canPhotographer": isFalse,
+            "created": isString,
+            "modified": isString
+          }
+        }));
   });
-
-  // test("POST /image with minimum data", () async {
-  //   final base64String =
-  //       base64Encode(await File("test/test_assets/test.jpg").readAsBytes());
-
-  //   final res = await harness.agent.post(
-  //     "/image",
-  //     body: {
-  //       "name": "My Picture",
-  //       "base64": base64String,
-  //     },
-  //   );
-
-  //   expect(
-  //       res,
-  //       hasResponse(200, body: {
-  //         "id": 1,
-  //         "name": "My Picture",
-  //         "description": null,
-  //         "created": isString, //e.g. 2020-04-26T10:20:32.552493Z
-  //         "modified": isString,
-  //         "captured": null,
-  //         "firstPubDate": null,
-  //         "coordinates": null,
-  //         "license": null,
-  //         "usesLeft": null,
-  //         "isPublicallyAdded": false,
-  //         //"category": {"id": 1},
-  //         "photographer": null,
-  //         "user": null,
-  //         "image": {
-  //           "id": 1,
-  //           "description": null,
-  //           //  "path": isString, //"e.g. "public/2020-04-26 10:20:32.548837Z.jpg",
-  //           "resolution": null,
-  //           "version": 1,
-  //           "created": isString,
-  //           "modified": isString,
-  //           "imageMetaData": {"id": 1}
-  //         }
-  //       }));
-
-  //   // expectResponse(res, 200);
-  // });
-
-  test("Photographer uploads picture with tags", () async {});
 }
