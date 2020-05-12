bdf437a70d0ab502895c2a24893e0da2bd73cbc2
Christian Mårtensson <chris.martensson@pm.me>
2020-05-12 16:19:51 +0200

    undid last commit

--- lib/controllers/tag_controller.dart
+++ lib/controllers/tag_controller.dart
@@ -1,20 +1,21 @@
 import 'package:bothnia_server/bothnia_server.dart';
 
 class TagController extends ManagedObjectController<Tag> {
-  TagController(ManagedContext context) : super(context) {
-    query = Query<Image>(context);
-  }
-  Query<Image> query;
+  TagController(ManagedContext context) : super(context);
+  // {
+  //   query = Query<Image>(context);
+  // }
+  // Query<Image> query;
 
-  @override
-  @Operation.get()
-  Future<Response> getImages() async {
-    query.join(object: (image) => image.photographer);
-    query.join(object: (image) => image.user);
-    query
-        .join(set: (image) => image.imageTags)
-        .join(object: (imageToTag) => imageToTag.tag);
+  // @override
+  // @Operation.get()
+  // Future<Response> getImages() async {
+  //   query.join(object: (image) => image.photographer);
+  //   query.join(object: (image) => image.user);
+  //   query
+  //       .join(set: (image) => image.imageTags)
+  //       .join(object: (imageToTag) => imageToTag.tag);
 
-    return Response.ok(await query.fetch());
-  }
+  //   return Response.ok(await query.fetch());
+  // }
 }
